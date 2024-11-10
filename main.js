38eb95d7d45fb99ae82335f325a4d839180f3026
rishimalviya <malviyaabhi059@gmail.com>
2024-11-11 01:06:49 +0530

    Initial commit for Three.js project

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color:'red',wireframe:true});
+const mesh = new THREE.Mesh(geometry,material);
+scene.add(mesh);
+
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight);
+camera.position.z= 3;
+scene.add(camera);
+
+const canvas = document.querySelector('canvas');
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(window.innerWidth,window.innerHeight);
+
+
+function animate(){
+  mesh.rotation.y += 0.01;
+  window.requestAnimationFrame(animate);
+  renderer.render(scene,camera);
+}
+
+animate();
